0d64ab1b2d7e6380138ee80d03600e9d11a66770
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-07-08 19:16:59 +0530

    Ip

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var ip = require("ipware");
+var userIp = ip().get_ip;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    console.log(userIp(req));
+    res.send(userIp(req));
 });
 
 module.exports = router;
