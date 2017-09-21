f644dcc2b36bae828bffdd7bb68800e7c5a2f331
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-09-21 10:39:37 +0530

    Setup google analytics

---
+++ public/javascripts/gtag.js
@@ -0,0 +1,11 @@
+/**
+ * Created by koteswarao on 21-09-2017.
+ */
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments)
+};
+gtag('js', new Date());
+
+gtag('config', 'UA-106742812-1');
