c8ac2864b58497a3011fc0944ef6d9bfbc0d7422
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-10-31 16:36:56 +0530

    Added gtag

---
+++ public/js/gtag.js
@@ -0,0 +1,11 @@
+/**
+ * Created by koteswarao on 31-10-2017.
+ */
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+    dataLayer.push(arguments)
+};
+gtag('js', new Date());
+
+gtag('config', 'UA-109032598-1');
